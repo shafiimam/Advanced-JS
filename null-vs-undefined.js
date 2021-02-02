b26351b8f654a4eb958f55d67e21a94fe0e24291
shafiimam <shafi15-8072@diu.edu.bd>
2021-02-02 22:29:18 +0600

    topics cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,11 @@
+// let pakhi;
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+const premik = { name: 'smart dude', phone: 34543 };
+console.log(premik.gf);
+
+let age = null;
+
+console.log(age);
---
+++ true-false.js
@@ -0,0 +1,10 @@
+const age = 0;
+
+const names = "";
+let name = null;
+console.log(name);
+if (name) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
