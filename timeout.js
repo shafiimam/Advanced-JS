896bbb98500ada86e0a713f2ddd5c68c63712c86
shafiimam <shafi15-8072@diu.edu.bd>
2021-02-07 01:03:38 +0600

    api done

---
+++ timeout.js
@@ -0,0 +1,17 @@
+// function doSomething() {
+//     console.log(3333);
+// }
+
+// console.log(3222);
+// setTimeout(doSomething, 3000);
+// console.log(4444);
+
+setInterval(() => {
+    console.log('doing it');
+}, 1000);
+
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
